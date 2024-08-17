3668b3b77c674d600b09c8b70574f066ada16470
Abdo ElHodaky <abdo.arh38@yahoo.com>
2024-08-17 17:01:14 +0000

    __

---
+++ craco.config.js
@@ -0,0 +1,34 @@
+// craco.config.js
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+  webpack: {
+    configure: webpackConfig => {
+      const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
+        ({ constructor }) => constructor && constructor.name === 'ModuleScopePlugin'
+      );
+
+      webpackConfig.resolve.plugins.splice(scopePluginIndex, 1);
+      webpackConfig['resolve'] = {
+        fallback: {
+           assert: false,
+            crypto: require.resolve('crypto-browserify'),
+            http: require.resolve('stream-http'),
+            https: require.resolve('https-browserify'),
+            os: require.resolve('os-browserify/browser'),
+            stream: require.resolve('stream-browserify'),
+            url:require.resolve('url/'),
+            buffer:require.resolve('buffer/'),
+            util:require.resolve('util/')
+        },
+      }
+      return webpackConfig;
+    },
+  },
+}
