a187a91ea5e779c2e0748809248572bef0c5b0b5
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-24 20:43:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -24,3 +24,19 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+window.addEventListener('load', () => {
+   if ("serviceWorker" in navigator) {
+      // Register a service worker hosted at the root of the
+      // site using the default scope.
+      navigator.serviceWorker.register("%PUBLIC_URL%/sw.js",{scope:"/"}).then(
+      (registration) => {
+         console.log("Service worker registration succeeded:", registration);
+      },
+      (error) => {
+         console.error(`Service worker registration failed: ${error}`);
+      },
+    );
+  } else {
+     console.error("Service workers are not supported.");
+  }
+      });
