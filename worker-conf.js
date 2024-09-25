9cd24cd778e9cd1aaa58c3dbc8db67b9d27144c5
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-25 17:21:44 +0300

    Update worker-conf.js

--- worker-conf.js
+++ worker-conf.js
@@ -1,8 +1,35 @@
 module.exports = {
 	globDirectory: 'public/',
 	globPatterns: [
-		'**/*.{css,png,jpg,ico,html,json,txt}'
+		'**/*.{css,png,jpg,ico,html,js,json,txt}'
 	],
-	swDest: 'sw.js',
-	swSrc: 'worker.js'
+	navigateFallback:"/offline.html",
+	runtimeCaching: [{
+    // Routing via a matchCallback function:
+    urlPattern: ({request, url}) => url.includes("cdn")==true,
+    handler: 'NetworkFirst',
+    options: {
+      cacheName: 'cdns',
+      expiration: {
+        maxEntries: 20
+      },
+      cacheableResponse:[0,200]
+    },
+  },{
+    urlPattern: ({request, url}) => url.includes("api")==true,
+    handler:"NetworkOnly",
+    method:"POST",
+    options:{
+      cacheName:"apCachePost",
+      cacheableResponse:[0,200],
+      backgroundSync:{
+       name:"Apisync",
+       options:{
+	maxRetentionTime:24*60*2
+       }
+      }
+    }
+  }],
+   swDest: 'sw.js',
+   swSrc: 'worker.js'
 };
