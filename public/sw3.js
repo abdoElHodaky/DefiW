9d664fbc218af09b7a60db1efca6f91f980551d3
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-25 16:09:24 +0300

    Update sw3.js

--- public/sw3.js
+++ public/sw3.js
@@ -2,15 +2,10 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
-const {registerRoute} = workbox.routing;
-const {CacheFirst} = workbox.strategies;
-const {CacheableResponse} = workbox.cacheableResponse;
-
-registerRoute(
-  ({request}) => request.url.includes("cdn") === true,
-  new CacheFirst({
-    plugins: [
-      cacheName:"cdns",
-      new CacheableResponsePlugin({statuses: [0, 200]})],
+// This will work!
+workbox.routing.registerRoute(
+  ({request}) => request.destination === 'image',
+  new workbox.strategies.CacheFirst({
+    cacheName:"images"
   })
 );
