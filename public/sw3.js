be20cf970082cca28a70badee5a92cd26fbf81fb
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-25 15:30:47 +0300

    Update sw3.js

--- public/sw3.js
+++ public/sw3.js
@@ -1,4 +1,14 @@
 importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+);
+
+// This will work!
+workbox.routing.registerRoute(
+  ({request}) => request.destination === 'image',
+  new workbox.strategies.CacheFirst()
+);
+
+/*importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/7.1.0/workbox-sw.js'
 );
 
@@ -70,7 +80,7 @@ const addToCache = function (request) {
     if(!event.request.url.startsWith('http')){
         event.waitUntil(addToCache(event.request));
     }
-});*/
+});
 if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
