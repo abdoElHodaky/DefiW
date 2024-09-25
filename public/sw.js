d1dfce0031b7033d201dbc7d53d8ae0ff642ab1b
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-25 08:01:37 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,10 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
 /*importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/7.1.0/workbox-sw.js'
 );
