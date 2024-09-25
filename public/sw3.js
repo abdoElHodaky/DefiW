38d11688b3bae3ab9dbdfe924dad76a71aee2ef0
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-25 17:37:27 +0300

    Update sw3.js

--- public/sw3.js
+++ public/sw3.js
@@ -5,3 +5,33 @@ workbox.setConfig({
   debug:true
 });
 workbox.loadModule("workbox-routing")
+workbox.loadModule("workbox-recipes")
+workbox.loadModule("workbox-background-sync")
+workbox.loadModule("workbox-strategies")
+
+
+const queue = new workbox.BackgroundSync.Queue('ApiQueue');
+
+self.addEventListener('fetch', event => {
+  // Add in your own criteria here to return early if this
+  // isn't a request that should use background sync.
+  if (event.request.method !== 'POST') {
+    return;
+  }
+
+  const bgSyncLogic = async () => {
+    try {
+      const response = await fetch(event.request.clone());
+      return response;
+    } catch (error) {
+      await queue.pushRequest({request: event.request});
+      return error;
+    }
+  };
+
+  event.respondWith(bgSyncLogic());
+});
+workbox.recipes.pageCache()
+workbox.recipes.offlineFallback()
+workbox.recipes.staticResourceCache();
+workbox.recipes.imageCache();
