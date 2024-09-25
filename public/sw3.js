fe27427a45e76e359bf1fc69e66ece6960423daf
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-25 16:24:06 +0300

    Update sw3.js

--- public/sw3.js
+++ public/sw3.js
@@ -1,6 +1,7 @@
-
+self.wrkbox={}
 self.addEventListener("install",(event)=>{
   importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+  self.wrkbox=workbox
 });
 // This will work!
 workbox.routing.registerRoute(
