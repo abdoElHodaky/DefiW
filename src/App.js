448e3f8d76c28a67e5d9de8cc58efc17cc290780
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-25 15:31:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
   }, []);
   useEffect(()=>{
     setTimeout(()=>{
-    navigator.serviceWorker.register("/sw2.js",{
+    navigator.serviceWorker.register("/sw3.js",{
       scope:"/",
       type:"module"
     })
