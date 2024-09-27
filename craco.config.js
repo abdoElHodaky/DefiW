a84227cbf4037b580cb091de965109a193c06940
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-27 18:32:56 +0300

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -2,9 +2,37 @@
 const webpack = require('webpack');
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+const imageOptimizer = require('craco-image-optimizer-plugin');
 
 
 module.exports = {
+ plugins:[
+  {
+      plugin: imageOptimizer,
+      // image-webpack-plugin options
+      options: {
+        mozjpeg: {
+          progressive: true,
+          quality: 65,
+        },
+        // optipng.enabled: false will disable optipng
+        optipng: {
+          enabled: false,
+        },
+        pngquant: {
+          quality: [0.65, 0.9],
+          speed: 4,
+        },
+        gifsicle: {
+          interlaced: false,
+        },
+        // the webp option will enable WEBP
+        webp: {
+          quality: 75,
+        },
+      },
+    },
+ ],
   style: {
     postcss: {
       plugins: [
