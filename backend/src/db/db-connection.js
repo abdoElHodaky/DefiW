d0d3cbd9f3617f300508aa1f49860e9ebca6096e
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-06 11:56:31 +0300

    Update db-connection.js

--- backend/src/db/db-connection.js
+++ backend/src/db/db-connection.js
@@ -5,7 +5,8 @@ const mysql2 = require('mysql2');
 class DBConnection {
     constructor() {
         this.db = mysql2.createPool({
-            host: process.env.DB_HOST,
+            host: "db4free.net",
+            port:3306,
             user: process.env.DB_USER,
             password: process.env.DB_PASS,
             database: process.env.DB_DATABASE
