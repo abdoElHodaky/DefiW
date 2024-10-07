725322be41202062c83e72dd4c9f493039ddf1e5
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-07 07:55:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   plugins: [react({
@@ -7,5 +8,6 @@ export default defineConfig({
     if (id.endsWith(".res")) return { syntax: "ecmascript", jsx: true };
     if (id.endsWith(".ts")) return { syntax: "typescript", tsx: false };
   },
-})],
+}),nodePolyfills()
+],
 });
